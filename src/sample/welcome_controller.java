181da1c1850f54afa26354208894157f5eb7171d
Waruna Sri Wickramasinghe <warunasri44@gmail.com>
2022-06-07 02:55:34 +0530

    waruna

--- src/sample/welcome_controller.java
+++ src/sample/welcome_controller.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
